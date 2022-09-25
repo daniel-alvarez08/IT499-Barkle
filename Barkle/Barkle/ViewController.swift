095967ae89c297cf89dcc75d9401ed43320abc58
daniel-alvarez08 <78048675+daniel-alvarez08@users.noreply.github.com>
2022-09-25 11:15:24 -0700

    Rename/struggles with messaging screen
    renamed buttons and changed mapping. I'm currently having issues trying to find a good way to make a messaging screen work. added code crashed my app so I'm starting again from the last save. will keep trying to find ways to do it.

--- Barkle/Barkle/ViewController.swift
+++ Barkle/Barkle/ViewController.swift
@@ -12,28 +12,28 @@ class ViewController: UIViewController {
     //references UI element (main image)
     @IBOutlet weak var animalPicOne: UIImageView!
     
-    var slayPic = 0
-    var noSlayPic = 2
+    var checkPic = 0
+    var xPic = 2
     
-    // when Slay is pressed, next picture is shown.
-    @IBAction func slayPressed(_ sender: UIButton) {
+    // when check is pressed, next picture is shown.
+    @IBAction func checkPressed(_ sender: UIButton) {
         
         // Var allows us to change images of animals at a later time.
         var animalPicSetOne = [#imageLiteral(resourceName: "Umbreon"), #imageLiteral(resourceName: "umbreon2"), #imageLiteral(resourceName: "umbreon3")]
         
         // Array of images of animals        shows random images from within Array
         animalPicOne.image = animalPicSetOne.randomElement()
-        slayPic += 1
+        checkPic += 1
     }
-    // When No Slay is pressed, the picture of another animal is shown.
-    @IBAction func noSlayPressed(_ sender: UIButton) {
+    // When X is pressed, the picture of another animal is shown.
+    @IBAction func xPressed(_ sender: UIButton) {
         
         // Var allows us to change images of animals at a later time.
         var animalPicSetTwo = [#imageLiteral(resourceName: "kaneki"), #imageLiteral(resourceName: "kaneki3"), #imageLiteral(resourceName: "kaneki2")]
         
         // Array of images of more animals   shows random images from within Array
         animalPicOne.image = animalPicSetTwo.randomElement()
-        noSlayPic -= 1
+        xPic -= 1
     }
 }
 
