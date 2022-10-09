136a7b42c690a9a442e8db0dd1ab8e9a7bd5a26b
daniel-alvarez08 <78048675+daniel-alvarez08@users.noreply.github.com>
2022-10-09 10:56:42 -0700

    Account page added!
    - Account page has been added, indicated by the 👤. 
    - Cancel button on account page works.
    - Save button crashes the app but it is being worked on.
    - Making small adjustments to picture placements.

---
+++ CAPSTONE/Barkle/AccountViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AccountViewController.swift
+//  Barkle
+//
+//  Created by Daniel Alvarez on 10/9/22.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+    @IBAction func cancel() {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ CAPSTONE/Barkle/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Barkle
+//
+//  Created by Daniel Alvarez on 9/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //references UI element (main image)
+    @IBOutlet weak var animalPicOne: UIImageView!
+    
+    var checkPic = 0
+    var xPic = 2
+    
+    // when check is pressed, next picture is shown.
+    @IBAction func checkPressed(_ sender: UIButton) {
+        
+        // Var allows us to change images of animals at a later time.
+        var animalPicSetOne = [#imageLiteral(resourceName: "Umbreon"), #imageLiteral(resourceName: "umbreon2"), #imageLiteral(resourceName: "umbreon3")]
+        
+        // Array of images of animals        shows random images from within Array
+        animalPicOne.image = animalPicSetOne.randomElement()
+        checkPic += 1
+    }
+    // When X is pressed, the picture of another animal is shown.
+    @IBAction func xPressed(_ sender: UIButton) {
+        
+        // Var allows us to change images of animals at a later time.
+        var animalPicSetTwo = [#imageLiteral(resourceName: "kaneki"), #imageLiteral(resourceName: "kaneki3"), #imageLiteral(resourceName: "kaneki2")]
+        
+        // Array of images of more animals   shows random images from within Array
+        animalPicOne.image = animalPicSetTwo.randomElement()
+        xPic -= 1
+    }
+}
+
